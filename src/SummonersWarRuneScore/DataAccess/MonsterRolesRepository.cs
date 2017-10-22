35a28cec25af35da3fef7d67493b68c469523a29
Shavv66 <25536624+Shavv66@users.noreply.github.com>
2017-10-22 22:48:14 +0100

    Re-added with gitignore

---
+++ src/SummonersWarRuneScore/DataAccess/MonsterRolesRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SummonersWarRuneScore.Domain;
+
+namespace SummonersWarRuneScore.DataAccess
+{
+	public class MonsterRolesRepository : IMonsterRolesRepository
+	{
+		private List<MonsterRole> mMonsterRoles;
+
+		public MonsterRolesRepository()
+		{
+			mMonsterRoles = new List<MonsterRole>
+			{
+				new MonsterRole("Support", RuneSet.Energy),
+				new MonsterRole("HP based damage dealer", RuneSet.Energy)
+			};
+		}
+
+		public MonsterRolesRepository(List<MonsterRole> monsterRoles)
+		{
+			mMonsterRoles = monsterRoles;
+		}
+
+		public List<MonsterRole> GetAll()
+		{
+			return mMonsterRoles;
+		}
+
+		public List<MonsterRole> GetByRuneSet(RuneSet runeSet)
+		{
+			return mMonsterRoles.Where(monsterRole => monsterRole.RuneSet == runeSet).ToList();
+		}
+
+		public void Add(MonsterRole monsterRole)
+		{
+			mMonsterRoles.Add(monsterRole);
+		}
+
+		public void Delete(MonsterRole monsterRole)
+		{
+			mMonsterRoles.Remove(monsterRole);
+		}
+	}
+}
