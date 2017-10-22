cf14d76b2dbae07c23f3ac4bc91ceb07a2a06e4a
Shavv66 <25536624+Shavv66@users.noreply.github.com>
2017-10-22 22:37:54 +0100

    Initial commit

---
+++ src/SummonersWarRuneScore/Domain/MonsterRole.cs
@@ -0,0 +1,14 @@
+﻿namespace SummonersWarRuneScore.Domain
+{
+	public class MonsterRole
+	{
+		public string Name { get; private set; }
+		public RuneSet RuneSet { get; private set; }
+		
+		public MonsterRole(string name, RuneSet runeSet)
+		{
+			Name = name;
+			RuneSet = runeSet;
+		}
+	}
+}
