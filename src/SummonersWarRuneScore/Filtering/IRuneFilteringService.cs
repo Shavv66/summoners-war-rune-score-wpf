6e15fa54f5acc884eedca1886937d10fc9f0d447
Shavv66 <25536624+Shavv66@users.noreply.github.com>
2018-02-22 20:58:00 +0000

    Started filtering

---
+++ src/SummonersWarRuneScore/Filtering/IRuneFilteringService.cs
@@ -0,0 +1,10 @@
+﻿using SummonersWarRuneScore.Domain;
+using System.Collections.Generic;
+
+namespace SummonersWarRuneScore.Filtering
+{
+	public interface IRuneFilteringService
+    {
+		List<Rune> FilterRunes(List<Rune> runes, Filter filter);
+    }
+}
