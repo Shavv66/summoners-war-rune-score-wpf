b177253a3e491ba49548e99e2036359118ef56e1
Shavv66 <25536624+Shavv66@users.noreply.github.com>
2018-03-18 20:15:30 +0000

    Extracted role manager user control from main window

---
+++ src/SummonersWarRuneScore/Client/UserControls/RoleManager/Events/RoleChangedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using SummonersWarRuneScore.Components.Domain;
+using System;
+
+namespace SummonersWarRuneScore.Client.UserControls.RoleManager.Events
+{
+	public class RoleChangedEventArgs : EventArgs
+	{
+		public MonsterRole ChangedRole { get; private set; }
+
+		public RoleChangedEventArgs(MonsterRole changedRole)
+		{
+			ChangedRole = changedRole;
+		}
+	}
+}
